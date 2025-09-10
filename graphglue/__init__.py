5fb96a2d737565cddbf59ebef20bfcb82a28accd
Daniele Bottazzi <79707228+daniele-bottazzi@users.noreply.github.com>
2025-09-10 11:38:29 +0200

    move code from src/graphglue/ into graphglue/

--- graphglue/__init__.py
+++ graphglue/__init__.py
@@ -1,23 +1,9 @@
-#!/usr/bin/env python
-
-#
-# This file is part of the `graphglue` Python module
-#
-# Copyright 2025
-# Heidelberg University Hospital
-#
-# File author(s): Daniele Bottazzi (daniele.bottazzi@mail.polimi.it)
-#
-# Distributed under the MIT license
-# See the file `LICENSE` or read a copy at
-# https://opensource.org/license/mit
-#
-
-"""Lightweight, flexible, minimal-dependencies Python package for creating, manipulating, and interoperating with diverse graph data structures."""
+from ._version import __version__
+from .core._graph import Graph
+from . import io as io
 
 __all__ = [
-    '__version__',
-    '__author__',
+    "__version__",
+    "Graph",
+    "io",
 ]
-
-from ._metadata import __author__, __version__
--- src/graphglue/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._version import __version__
-from .core._graph import Graph
-from . import io as io
-
-__all__ = [
-    "__version__",
-    "Graph",
-    "io",
-]
