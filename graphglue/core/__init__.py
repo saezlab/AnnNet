e9e5adf20c73ece501a62d72bec31899451341b5
L1BOLL <ynaciri808@gmail.com>
2025-10-04 19:21:39 +0100

    add icnidencegraph to init

--- graphglue/core/__init__.py
+++ graphglue/core/__init__.py
@@ -2,5 +2,6 @@ from .structure import *
 from .metadata import *
 from .views import *
 from .adapter import IncidenceAdapter
+from .incgraph import IncidenceGraph
 
-__all__ = ["structure", "metadata", "views", "IncidenceAdapter"]
+__all__ = ["structure", "metadata", "views", "IncidenceAdapter", "IncidenceGraph"]
